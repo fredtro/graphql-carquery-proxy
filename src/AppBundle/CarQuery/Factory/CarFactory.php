d8da49178beda2bd9aab06dd1f89ed67a367eef3
Frederik Trostorf <fredtrostorf@gmail.com>
2018-05-06 09:33:28 +0200

    feat: add some more info on car result

--- src/AppBundle/CarQuery/Factory/CarFactory.php
+++ src/AppBundle/CarQuery/Factory/CarFactory.php
@@ -3,21 +3,25 @@
 
 namespace AppBundle\CarQuery\Factory;
 
-
 class CarFactory
 {
     /**
      * @param array $data
      * @return array
      */
-    public static function create (array $data)
+    public static function create(array $data)
     {
         return [
             'id' => $data['model_id'],
             'name' => $data['model_name'],
             'make' => $data['make_display'],
-            'power' => (int) $data['model_engine_power_ps'],
-            'topSpeed' => (int )$data['model_top_speed_kph']
+            'power' => isset($data['model_engine_power_hp']) ?
+                (int)$data['model_engine_power_hp'] : (int)$data['model_engine_power_ps'],
+            'topSpeed' => (int )$data['model_top_speed_kph'],
+            'fuelType' => $data['model_engine_fuel'],
+            'length' => $data['model_length_mm'],
+            'width' => $data['model_width_mm'],
+            'height' => $data['model_height_mm']
         ];
     }
 }
