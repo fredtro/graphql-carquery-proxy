d7e79355f59fca6a5d9351e5568bfa8e87358740
Frederik Trostorf <fredtrostorf@gmail.com>
2018-05-06 10:25:45 +0200

    feat: add some testing on carfactory

--- src/AppBundle/CarQuery/Factory/CarFactory.php
+++ src/AppBundle/CarQuery/Factory/CarFactory.php
@@ -6,22 +6,50 @@ namespace AppBundle\CarQuery\Factory;
 class CarFactory
 {
     /**
+     * @var array
+     */
+    public static $dataFields = [
+        'model_id',
+        'model_name',
+        'make_display',
+        'model_top_speed_kph',
+        'model_engine_fuel',
+        'model_length_mm',
+        'model_width_mm',
+        'model_height_mm'
+    ];
+
+    /**
      * @param array $data
      * @return array
      */
     public static function create(array $data)
     {
+        static::validate($data);
+
         return [
             'id' => $data['model_id'],
             'name' => $data['model_name'],
             'make' => $data['make_display'],
-            'power' => isset($data['model_engine_power_hp']) ?
-                (int)$data['model_engine_power_hp'] : (int)$data['model_engine_power_ps'],
+            'power' => (isset($data['model_engine_power_hp']) ? (int)$data['model_engine_power_hp'] :
+                ((isset($data['model_engine_power_ps'])) ? (int)$data['model_engine_power_ps'] : 0)),
             'topSpeed' => (int )$data['model_top_speed_kph'],
             'fuelType' => $data['model_engine_fuel'],
-            'length' => $data['model_length_mm'],
-            'width' => $data['model_width_mm'],
-            'height' => $data['model_height_mm']
+            'length' => (int)$data['model_length_mm'],
+            'width' => (int)$data['model_width_mm'],
+            'height' => (int)$data['model_height_mm']
         ];
+    }
+
+    /**
+     * check if all fields included - except power, this is handled with a 0 in code
+     * @param $data
+     */
+    public static function validate($data)
+    {
+        $missingKeys = array_diff(static::$dataFields, array_keys($data));
+        if (count($missingKeys) > 0) {
+            throw new \RuntimeException('Missing data.');
+        }
     }
 }
---
+++ tests/AppBundle/CarQuery/CarFactoryTest.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace Tests\AppBundle\CarQuery;
+use AppBundle\CarQuery\Factory\CarFactory;
+use PHPUnit\Framework\TestCase;
+
+
+class CarFactoryTest extends TestCase
+{
+    /**
+     * mocked cardata from api
+     * @var array
+     */
+    private $carData = [
+        'model_id' => 11459,
+        'model_name' => 'Viper',
+        'make_display' => 'Dodge',
+        'model_engine_power_hp' => 506,
+        'model_top_speed_kph' => 314,
+        'model_engine_fuel' => 'Gasoline',
+        'model_length_mm' => 4470,
+        'model_width_mm' => 1950,
+        'model_height_mm' => 1220
+    ];
+
+    /**
+     * test if data is converted correctly by factory
+     */
+    public function testCreate()
+    {
+        $expected = [
+            'id' => 11459,
+            'name' => 'Viper',
+            'make' => 'Dodge',
+            'power' => 506,
+            'topSpeed' => 314,
+            'fuelType' => 'Gasoline',
+            'length' => 4470,
+            'width' => 1950,
+            'height' => 1220
+        ];
+
+        $result = CarFactory::create($this->carData);
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testMissingDataThrowsException()
+    {
+        $data = $this->carData;
+        unset($data['model_id']);
+        CarFactory::create($data);
+    }
+}
